e326e9a2e9c1fd9aee9e13ceb8fdd5ae065907aa
yanlu <1280065541@qq.com>
2021-03-12 10:02:33 +0800

    增加手写实现

---
+++ 手写实现/instanceof实现.js
@@ -0,0 +1,22 @@
+function newInstanceof(val, type) {
+    //直接用typeof判断基础数据类型，不是直接返回false
+    if (val === null || typeof val !== 'object') {
+        return false
+    }
+    var proto = Object.getPrototypeOf(val) //getPrototypeOf能够拿到参数的原型对象
+    while (true) {
+        if (proto === null) {
+            //没找到就返回false
+            return false
+        }
+        if (proto === type.prototype) {
+            //找到相同对象的原型，返回true
+            return true
+        }
+        proto = Object.getPrototypeOf(proto)
+    }
+}
+
+console.log(newInstanceof(new Number(123), Number))
+//不能判断基础数据类型
+console.log(newInstanceof(123, Number))
